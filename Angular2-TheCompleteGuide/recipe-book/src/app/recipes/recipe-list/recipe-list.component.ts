70e55162c7fc7aa8d850782df613ba3b10d88706
Hung Quach <quachio@icloud.com>
2016-07-23 10:34:06 -0700

    refractor code in recipe-lis

--- Angular2-TheCompleteGuide/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular2-TheCompleteGuide/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,6 @@
-import {
-  Component,
-  OnInit,
-  EventEmitter,
-  Output,
-} from '@angular/core';
-import {
-  Recipe
-} from '../recipe';
-import {
-  RecipeItemComponent
- } from './recipe-item.component';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe';
+import { RecipeItemComponent } from './recipe-item.component';
 
 @Component({
   moduleId:    module.id,
