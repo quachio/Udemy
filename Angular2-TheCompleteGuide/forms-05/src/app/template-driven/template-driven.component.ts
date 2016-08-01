9e28e14860d382a7abd5a0bac1435b1bf9322b4f
Hung Quach <quachio@icloud.com>
2016-08-01 16:31:11 -0700

    add radio button in form

--- Angular2-TheCompleteGuide/forms-05/src/app/template-driven/template-driven.component.ts
+++ Angular2-TheCompleteGuide/forms-05/src/app/template-driven/template-driven.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NgForm    } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  moduleId:     module.id,
-  selector:    'template-driven',
+  moduleId:  module.id,
+  selector: 'template-driven',
   templateUrl: 'template-driven.component.html',
-  styles:       [`
+  styles: [`
     .ng-invalid {
       border: 1px solid red;
     }
@@ -17,11 +17,18 @@ export class TemplateDrivenComponent {
     username: 'Max',
     email: 'chris@test.com',
     password: 'a1s2d3f4',
-  }
+    gender: 'male',
+  };
+
+  gender = [
+    'male',
+    'female',
+  ];
 
   onSubmit(form: NgForm){
-    console.log('It Works!');
-    console.log(form);
-    console.log(this.user);
+    //console.log('It Works!');
+    //console.log(form);
+    //console.log(this.user);
+    console.log(form.value);
   }
 }
